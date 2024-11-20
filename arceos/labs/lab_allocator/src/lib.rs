ca79bee17d6c9af95818e14b8c6a192d36463eb8
Xiangyu Tu <hsiangyutu@foxmail.com>
2024-11-20 23:02:27 +0800

    slab with tlsf allocator

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -2,42 +2,94 @@
 
 #![no_std]
 #![allow(unused_variables)]
+#![feature(allocator_api)]
 
-use allocator::{BaseAllocator, ByteAllocator, AllocResult};
+use allocator::{BaseAllocator, ByteAllocator, AllocResult, AllocError};
+use slab::Heap;
 use core::ptr::NonNull;
 use core::alloc::Layout;
 
-pub struct LabByteAllocator;
+mod slab;
+
+pub struct LabByteAllocator {
+    inner: Option<Heap>
+}
+
 
 impl LabByteAllocator {
     pub const fn new() -> Self {
-        Self
+        Self { inner: None }
+    }
+
+    fn inner_mut(&mut self) -> &mut Heap {
+        self.inner.as_mut().unwrap()
+    }
+
+    fn inner(&self) -> &Heap {
+        self.inner.as_ref().unwrap()
     }
 }
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        unimplemented!();
+        self.inner = unsafe { Some(Heap::new(start, size)) };
     }
+
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
-        unimplemented!();
+        unsafe {
+            self.inner_mut().add_memory(start, size);
+        }
+        Ok(())
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
-        unimplemented!();
+        self.inner_mut()
+            .allocate(layout)
+            .map(|addr| unsafe { NonNull::new_unchecked(addr as *mut u8) })
+            .map_err(|_| AllocError::NoMemory)
     }
+
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
-        unimplemented!();
+        unsafe { self.inner_mut().deallocate(pos.as_ptr() as usize, layout) }
     }
+
     fn total_bytes(&self) -> usize {
-        unimplemented!();
+        self.inner().total_bytes()
     }
+
     fn used_bytes(&self) -> usize {
-        unimplemented!();
+        self.inner().used_bytes()
     }
+
     fn available_bytes(&self) -> usize {
-        unimplemented!();
+        self.inner().available_bytes()
+    }
+}
+
+// Stdout for debugging
+struct Stdout;
+use core::fmt::Write;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for c in s.chars() {
+            #[allow(deprecated)]
+            sbi_rt::legacy::console_putchar(c as usize);
+        }
+    
+        Ok(())
+    }
+}
+
+pub fn print(args: core::fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
     }
 }
